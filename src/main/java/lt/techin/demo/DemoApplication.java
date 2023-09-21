1cc48c4dd2b501d1282ac1f69bb3f2f5c824a30d
Julius <jzabulenas@protonmail.com>
2023-09-21 15:40:28 +0300

    Add Spring Boot project

---
+++ src/main/java/lt/techin/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package lt.techin.demo;
+
+import lt.techin.demo.controllers.PersonController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/lt/techin/demo/controllers/PersonController.java
@@ -0,0 +1,35 @@
+package lt.techin.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class PersonController {
+
+    private final ArrayList<String> people = new ArrayList<>(Arrays.asList("Jurgis", "Antanas",
+            "Aloyzas", "Martynas"));
+
+    @GetMapping("/people")
+    public List<String> getPeople() {
+        return people;
+    }
+
+    @GetMapping("/people/{index}")
+    public String getPerson(@PathVariable int index) {
+        return people.get(index);
+    }
+
+    @PostMapping("/people")
+    public void addPerson(@RequestBody String name) {
+        people.add(name);
+    }
+
+    @PutMapping("/people/{index}")
+    public void updatePerson(@PathVariable int index, @RequestBody String name) {
+        people.set(index, name);
+    }
+}
