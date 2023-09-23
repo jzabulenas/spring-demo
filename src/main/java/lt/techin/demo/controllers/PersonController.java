aacaff143d77afe09b4191418cc9f2f8385df9c9
Julius <jzabulenas@protonmail.com>
2023-09-23 19:20:18 +0300

    Doing something...

--- src/main/java/lt/techin/demo/controllers/PersonController.java
+++ src/main/java/lt/techin/demo/controllers/PersonController.java
@@ -1,5 +1,8 @@
 package lt.techin.demo.controllers;
 
+import lt.techin.demo.entities.Person;
+import lt.techin.demo.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,30 +13,31 @@ import java.util.List;
 @RestController
 public class PersonController {
 
-    private final ArrayList<String> people = new ArrayList<>(Arrays.asList("Jurgis", "Antanas", "Aloyzas", "Martynas"));
+    @Autowired
+    private PersonRepository personRepository;
 
-    @GetMapping("/people")
-    public List<String> getPeople() {
-        return people;
-    }
-
-    @GetMapping("/people/{index}")
-    public String getPerson(@PathVariable int index) {
-        return people.get(index);
-    }
+//    @GetMapping("/people")
+//    public List<String> getPeople() {
+//        return people;
+//    }
+//
+//    @GetMapping("/people/{index}")
+//    public String getPerson(@PathVariable int index) {
+//        return people.get(index);
+//    }
 
     @PostMapping("/people")
-    public void addPerson(@RequestBody String name) {
-        people.add(name);
+    public void addPerson(@RequestBody Person person) {
+        personRepository.save(person);
     }
 
-    @PutMapping("/people/{index}")
-    public void updatePerson(@PathVariable int index, @RequestBody String name) {
-        people.set(index, name);
-    }
-
-    @DeleteMapping("/people/{index}")
-    public void deletePerson(@PathVariable int index) {
-        people.remove(index);
-    }
+//    @PutMapping("/people/{index}")
+//    public void updatePerson(@PathVariable int index, @RequestBody String name) {
+//        people.set(index, name);
+//    }
+//
+//    @DeleteMapping("/people/{index}")
+//    public void deletePerson(@PathVariable int index) {
+//        people.remove(index);
+//    }
 }
--- src/main/java/lt/techin/demo/entities/Person.java
+++ src/main/java/lt/techin/demo/entities/Person.java
@@ -12,4 +12,16 @@ public class Person {
     private int id;
     private String firstName;
     private String lastName;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
