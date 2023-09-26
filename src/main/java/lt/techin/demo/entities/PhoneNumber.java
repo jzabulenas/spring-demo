cdc78ad71d2787509aa220cfb8ae7d103208fcc1
Julius <jzabulenas@protonmail.com>
2023-09-26 15:22:08 +0300

    Implement PhoneNumber class

---
+++ src/main/java/lt/techin/demo/entities/PhoneNumber.java
@@ -0,0 +1,32 @@
+package lt.techin.demo.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Phone_numbers")
+public class PhoneNumber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String phoneNumber;
+    @ManyToOne
+    @JoinColumn(name = "Person_id")
+    private Person person;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
