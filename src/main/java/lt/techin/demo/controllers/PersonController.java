14e090b665793b5a32fa2610781dc132b56d3d8d
Julius <jzabulenas@protonmail.com>
2023-09-26 13:29:23 +0300

    Change to construction injection

--- src/main/java/lt/techin/demo/controllers/PersonController.java
+++ src/main/java/lt/techin/demo/controllers/PersonController.java
@@ -11,8 +11,12 @@ import java.util.Optional;
 @RestController
 public class PersonController {
 
+    private final PersonRepository personRepository;
+
     @Autowired
-    private PersonRepository personRepository;
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     @GetMapping("/people")
     public List<Person> getPeople() {
